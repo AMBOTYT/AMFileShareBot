b6089c49317e3792371f2f6aadba1e1252c3bd75
AMBOTYT <142726836+AMBOTYT@users.noreply.github.com>
2023-11-14 23:25:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,66 @@
+from os import getenv
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+# ---------------------- ᴄᴏɴғɪɢ ---------------------- #
+
+API_ID = int(getenv("API_ID", "12227067"))
+API_HASH = getenv("API_HASH", "b463bedd791aa733ae2297e6520302fe")
+BOT_TOKEN = getenv("BOT_TOKEN", "6335671691:AAHuSx_o23LPKGs5XsMm64IJfz9KztsPT7U")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002086023824"))
+OWNER_ID = list(map(int, getenv("SUDO_USERS", "6204761408 5360305806 6029628148").split()))
+DB_URI = getenv("DATABASE_URL", "postgres://zvvrdoji:kSdhyo9vmR9lAKFnYs4XnFB7bGqxPNyo@snuffleupagus.db.elephantsql.com/zvvrdoji")
+FORCE_SUB_CHANNEL = int(getenv("FORCE_SUB_CHANNEL", "-1001987535452"))
+BOT_WORKERS = int(getenv("BOT_WORKERS", "5"))
+
+# --------- ʟɪɴᴋ sʜᴏʀᴛɴᴇʀ ᴄᴏɴᴠᴇʀᴛᴇʀ ---------
+URL_SHORTNER_API = getenv("URL_SHORTNER_API", "http://urlshortx.com/api?api")
+URL_SHORTNER_API_KEY = getenv("URL_SHORTNER_API_KEY", "16LYjmErwNek2gMQkNrkLm2i1QVhjmxSRo")
+
+
+default_custom_caption = """
+📁 {file_caption}
+━━━━━━━━━━━━━━━━━━━━━━━━━━━   
+ɪғ ʏᴏᴜ ʟɪᴋᴇ ᴠɪᴅᴇᴏ ᴛʜᴀɴ ᴘʟᴇᴀsᴇ 
+ᴀᴅᴅ sᴏᴍᴇ ᴍᴇᴍʙᴇʀ ᴀɴᴅ sʜᴀʀᴇ ᴛʜᴇ ʟɪɴᴋ
+━━━━━━━━━━━━━━━━━━━━━━━━━━━
+"""
+CUSTOM_CAPTION = getenv("CUSTOM_CAPTION", default_custom_caption)
+
+# --» sᴇᴛ ᴛʀᴜᴇ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴘʀᴇᴠᴇɴᴛ ᴜsᴇʀs ғʀᴏᴍ ғᴏʀᴡᴀʀᴅɪɴɢ ғɪʟᴇs ғʀᴏᴍ ʙᴏᴛ
+if getenv("PROTECT_CONTENT", None) == 'True':
+    PROTECT_CONTENT = True
+else:
+    PROTECT_CONTENT = False
+
+
+# --> sᴇᴛ ᴛʀᴜᴇ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴅɪsᴀʙʟᴇ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛs sʜᴀʀᴇ ʙᴜᴛᴛᴏɴ
+if getenv("DISABLE_CHANNEL_BUTTON", None) == 'True':
+    DISABLE_CHANNEL_BUTTON = True
+else:
+    DISABLE_CHANNEL_BUTTON = False
+
+
+
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
